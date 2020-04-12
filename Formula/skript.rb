c70a0663ad8dbff469c529cfce9b13874991ff0a
Alexander Sulim <hello@sul.im>
2020-04-12 07:39:40 +0200

    Release skript v0.3.0

--- Formula/skript.rb
+++ Formula/skript.rb
@@ -1,10 +1,10 @@
 class Skript < Formula
   desc 'A command-line tool to bootstrap scripts according to the normalized script pattern'
   homepage 'https://github.com/soulim/skript'
-  version '0.2.2'
+  version '0.3.0'
   bottle :unneeded
-  url 'https://github.com/soulim/skript/releases/download/v0.2.2/skript_0.2.2.tar.gz'
-  sha256 '230501b178782770ad4a4469834c699e029e619080576267c87c19c86fa4051a'
+  url 'https://github.com/soulim/skript/releases/download/v0.3.0/skript_0.3.0.tar.gz'
+  sha256 '99ab9c03f43a8d690054c0e03aa92445cb885dd27e322fd68122359c5086cee2'
   # TODO: Enable tests
   # test do
   #   help_text = shell_output("#{bin}/skript --help")
